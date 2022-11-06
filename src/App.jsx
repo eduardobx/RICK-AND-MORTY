cd288d9cad4bc040927e3601ca13c9d506ce0eb6
Eduardo Burgos Humbo <edu_94_bhf@hotmail.com>
2022-11-06 00:41:31 -0500

    final

--- src/App.jsx
+++ src/App.jsx
@@ -5,77 +5,84 @@ import CharacterUrl from "./components/CharacterUrl";
 import LocationFeatures from "./components/LocationFeatures";
 import Name from "./components/Name";
 
-
 function App() {
   const [location, setLocation] = useState([]);
-  const [idLocation,setIdLocation]=useState("")
-  const [name,setName]=useState([])
-  
+  const [idLocation, setIdLocation] = useState("");
+  const [name, setName] = useState([]);
 
   useEffect(() => {
-     const i=Math.floor(Math.random ()  *100)
-     axios.get(`https://rickandmortyapi.com/api/location/${i}`)
-      .then((res) => setLocation(res.data))
-    
+    const i = Math.floor(Math.random() * 100);
+    axios
+      .get(`https://rickandmortyapi.com/api/location/${i}`)
+      .then((res) => setLocation(res.data));
   }, []);
 
-  const seacrhType=()=>{
+  const seacrhType = () => {
+    axios
+      .get(`https://rickandmortyapi.com/api/location/${idLocation}`)
+      .then((res) => setLocation(res.data));
 
-    axios.get(`https://rickandmortyapi.com/api/location/${idLocation}`)
-    .then((res) => setLocation(res.data))
-
-    if(idLocation>150){
-      alert("Id no encontrado")
-    }else if(isNaN(idLocation)){
-      alert (" no es un id")
+    if (idLocation > 150) {
+      alert("Id no encontrado");
+    } else if (isNaN(idLocation)) {
+      alert(" no es un id");
     }
-    const arrayname=[...name]
-    arrayname.push(location.name)
-    setName(arrayname)
+    const arrayname = [...name];
+    arrayname.push(location.name);
+    setName(arrayname);
 
-     setIdLocation("")
-  }
+    setIdLocation("");
+  };
 
-    console.log(name)
-  
- const idCharacter=(e)=>{
-    setIdLocation(e.target.value)
+  console.log(name);
 
- }
+  const idCharacter = (e) => {
+    setIdLocation(e.target.value);
+  };
+  const newlocation = () => {
+    alert("hola");
+  };
+  const suggestions = () => {
+    if (idLocation !== "") {
+      return (
+        <form>
+          {name.map((index) => (
+            <div key={index} onClick={newlocation}>
+              {index}
+            </div>
+          ))}
+        </form>
+      );
+    }
+  };
 
- const  suggestions=()=>{
-       if(idLocation!==""   ){
-      return  <form >
-      {name.map(index =>(  <div>{index}</div>))}
-     </form>
-       } 
- }
- 
-   
-console.log(location)
+  console.log(location);
   return (
     <div className="App">
-   <div className="conteiner-main">
-     <header>
-     <div className="img-back">
-      <div>
-     <h1>RICK AND MORTY wikky</h1>
-     <input  type="text" value={idLocation}  onChange={idCharacter} placeholder="write a location id"/>
-     <button onClick={seacrhType}>seacrh</button>
-      {suggestions()}
+      <div className="conteiner-main">
+        <header>
+          <div className="img-back">
+            <div>
+              <h1>RICK AND MORTY wikky</h1>
+              <input
+                type="text"
+                value={idLocation}
+                onChange={idCharacter}
+                placeholder="write a location id"
+              />
+              <button onClick={seacrhType}>seacrh</button>
+              {suggestions()}
+            </div>
+          </div>
+          <LocationFeatures location={location} />
+        </header>
 
-     </div>
-     </div>
-      <LocationFeatures location={location}/>
-     </header>
-    
-     <main className=" conteiner-fearures">
-     { location.residents?.map(urlCharcter=> (
-      <CharacterUrl url={urlCharcter} key={urlCharcter}/>) )
-     }
-     </main> 
-  </div>
-  
+        <main className=" conteiner-fearures">
+          {location.residents?.map((urlCharcter) => (
+            <CharacterUrl url={urlCharcter} key={urlCharcter} />
+          ))}
+        </main>
+      </div>
     </div>
   );
 }
